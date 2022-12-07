afe7f5f5f8562263942fd5a75dc24780ab4f3a94
pablo.mandile@bue.edu.ar <pablo.mandile@bue.edu.ar>
2022-12-07 16:16:05 -0300

    Estructura y primeros componentes proyecto listado de tareas

---
+++ src/componentes/Tarea.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../css/Tarea.css';
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+function Tarea({texto, completada}){
+  return (
+    <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
+      <div className="tarea-texto">
+        {texto}
+      </div>
+      <div className="tarea-contenedor-icono">
+        <AiOutlineCloseCircle className='tarea-icono'  />
+      </div>
+    </div>
+  );
+}
+
+export default Tarea;
